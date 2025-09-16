c5d76892fc343658fe21157e7f1189a6edcf8c99
Linnea <linneahartsuyker@imarc.com>
2025-09-16 13:41:34 -0400

    Testing linking in the bso event calendar

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,27 +1,29 @@
 import { createApp, defineAsyncComponent } from 'vue'
 import directionals from './directives/vDirectionals.js'
 import scrolllock from './directives/vScrolllock.js'
+import Calendar from 'henry-search'
 
 // Necessary for Craft vite plugin
 import '../styles/index.scss'
 
 createApp({
-  components: {
-    PAccordion: defineAsyncComponent(() => import('./components/PAccordion.vue')),
-    PDismissable: defineAsyncComponent(() => import('./components/PDismissable.vue')),
-    PLazy: defineAsyncComponent(() => import('./components/PLazy.vue')),
-    POpenable: defineAsyncComponent(() => import('./components/POpenable.vue')),
-    PTabs: defineAsyncComponent(() => import('./components/PTabs.vue')),
-    PDirectionalKeys: defineAsyncComponent(() => import('./components/PDirectionalKeys.vue')),
+    components: {
+        PAccordion: defineAsyncComponent(() => import('./components/PAccordion.vue')),
+        PDismissable: defineAsyncComponent(() => import('./components/PDismissable.vue')),
+        PLazy: defineAsyncComponent(() => import('./components/PLazy.vue')),
+        POpenable: defineAsyncComponent(() => import('./components/POpenable.vue')),
+        PTabs: defineAsyncComponent(() => import('./components/PTabs.vue')),
+        PDirectionalKeys: defineAsyncComponent(() => import('./components/PDirectionalKeys.vue')),
 
-    PSelect: defineAsyncComponent(() => import('@vueform/multiselect/themes/default.css') && import('@vueform/multiselect')),
-    PSlider: defineAsyncComponent(() => import('./components/PSlider.vue')),
-    PYouTube: defineAsyncComponent(() => import('./components/PYouTube.vue')),
+        PSelect: defineAsyncComponent(() => import('@vueform/multiselect/themes/default.css') && import('@vueform/multiselect')),
+        PSlider: defineAsyncComponent(() => import('./components/PSlider.vue')),
+        PYouTube: defineAsyncComponent(() => import('./components/PYouTube.vue')),
 
-    ElementCollection: defineAsyncComponent(() => import('./components/ElementCollection.vue')),
-  },
-  directives: {
-    directionals,
-    scrolllock,
-  },
+        ElementCollection: defineAsyncComponent(() => import('./components/ElementCollection.vue')),
+        Calendar,
+    },
+    directives: {
+        directionals,
+        scrolllock,
+    },
 }).mount('#app')
