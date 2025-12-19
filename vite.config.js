6135cad9a2be565da66151683498cd4143269b92
Linnea <linneahartsuyker@imarc.com>
2025-12-19 12:36:00 -0500

    Updating header styles

--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,49 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitrine from '@imarc/vitrine'
 
-export default defineConfig({
-    build: {
-        manifest: true,
-        outDir: 'web/dist',
-        assetsDir: '.',
-        rollupOptions: {
-            input: [
-                './resources/styles/index.scss',
-                './resources/js/index.js',
-            ],
-        },
-    },
-    plugins: [
-        vue(),
-        vitrine({
-            basePaths: [
-
-                /**
-         * This should be set to the base directory for your front end files.
-         */
-                'resources',
-            ],
+export default defineConfig(async ({ command }) => {
+    const config = {
+        build: {
+            manifest: true,
             outDir: 'web/dist',
-            includes: [
-                /**
-         * These are the entry points to include. These will also need to get
-         * included into your project.
-         */
-                '/resources/js/index.js',
-            ],
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        }
-    },
-    server: {
-        cors: true,
-        origin: 'https://padstone.imarc.io'
-    },
-    //base: '/dist/'
+            assetsDir: '.',
+            rollupOptions: {
+                input: [
+                    './resources/styles/index.scss',
+                    './resources/js/index.js',
+                ],
+            },
+        },
+        plugins: [
+            vue(),
+            vitrine({
+                basePaths: [
+
+                    /**
+             * This should be set to the base directory for your front end files.
+             */
+                    'resources',
+                ],
+                outDir: 'web/dist',
+                includes: [
+                    /**
+             * These are the entry points to include. These will also need to get
+             * included into your project.
+             */
+                    '/resources/js/index.js',
+                ],
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+            }
+        },
+        server: {
+            cors: true,
+            origin: 'https://padstone.imarc.io'
+        },
+        base: command === 'serve' ? '' : '/dist/',
+    }
+    return config
 })
